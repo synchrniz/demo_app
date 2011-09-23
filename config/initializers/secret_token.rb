e32b005f38456fd78d5396e183bb1627bd693f1d
synchrniz@gmail.com <synchrniz@unknown_c8-bc-c8-cd-1a-6d.gateway.2wire.net>
2011-09-23 12:43:04 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '7a04608cdccef5f8f5ad9d754bcf63bbfd6629ee41fe9dccda7e0ed55fdd72ba6cb8643d5a2cb568194f38a931ea45c7deec2d8ffbef19c27378b98205ad46e3'
